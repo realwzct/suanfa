f079f804367c84962fb852947e8c5d0c44afdf4f
realwzc <realwzc@outlook.com>
2021-12-29 17:17:28 +0800

    testlink

--- reverselink.c
+++ reverselink.c
@@ -48,7 +48,7 @@ link *recursive_reverse(link *head)
         //递归每退出一层，函数中 head 指针的指向都会发生改变，都指向上一个节点。
         //每退出一层，都需要改变 head->next 节点指针域的指向，同时令 head 所指节点的指针域为 NULL。
         head->next->next = head;
-        head->next = NULL;
+        head->next = NULL;//不防空的最后不好判断
         //每一层递归结束，都要将新的头指针返回给上一层。由此，即可保证整个递归过程中，能够一直找得到新链表的表头。
         return new_head;
     }
